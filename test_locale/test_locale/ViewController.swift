fce9adc807f3e1006ab4c3507c983dd9f5774408
Egor Yaroshuk <eg.yaroshuk@gmail.com>
2022-10-20 16:20:21 +0300

    Base commit

---
+++ test_locale/test_locale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_locale
+//
+//  Created by Егор Ярошук on 20.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
